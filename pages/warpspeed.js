3f83c9961ad4cc8fc9486d1e081f77efe5da8442
Matthew Knudsen <mknudsen01@gmail.com>
2020-06-26 13:37:50 -0700

    Add page for warpspeed sketch

---
+++ pages/warpspeed.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SketchLayout from "../components/SketchLayout";
+import warpspeedSketch from "../static/sketches/warpspeed";
+
+const Warpspeed = () => {
+  return (
+    <>
+      <SketchLayout sketch={warpspeedSketch} title="Warpspeed" />
+    </>
+  );
+};
+
+export default Warpspeed;
