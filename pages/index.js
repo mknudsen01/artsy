57156a2e96d17328610884f5e3a7f4b4994a30f7
Matthew Knudsen <mknudsen01@gmail.com>
2020-06-26 10:01:33 -0700

    Add index page with link to current sketches

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+import Head from "../components/head";
+
+const Home = () => (
+  <div>
+    <Head title="Home" />
+    <h2>Computational Design experiments</h2>
+    <p>
+      I've never put in the effort to learn how to paint well, design well, draw
+      well. But I'm still interested in making art. So, recently I've been
+      learning a little bit about generative art, using code to create designs.
+    </p>
+    <p>
+      Below are a few examples, which I'll add to as I code up new algorithms.
+      Check 'em out and holler at me with any cool designs you make. ✌️
+    </p>
+    <h3>
+      <Link href="/noise">
+        <a>Perlin Noise</a>
+      </Link>
+    </h3>
+  </div>
+);
+
+export default Home;
